13c96e40bb344af43b804546c60e6081d9c86adb
karxg <karim4420@aol.com>
2024-08-01 18:45:24 +0300

    fix100

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -4,6 +4,7 @@ from typing import Any, Sequence, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """"Duck typing - first element of a sequence"""
     if lst:
         return lst[0]
     else:
